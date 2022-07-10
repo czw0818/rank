5cbd28263d402e7a951e45ad802642ec034a4ede
czw0818 <1352677769@qq.com>
2022-07-10 12:56:00 +0800

    *这不是一个可执行的版本*初步完成TokenStream

--- src/frontend/analyzer.rs
+++ src/frontend/analyzer.rs
@@ -1,14 +1,46 @@
-use super::tokens::Token;
-use logos::Logos;
+use std::{iter::Peekable, ops::Deref};
 
-struct TokenStream<'a>{
-    peek:Option<Token>,
-    token_gen:logos::Lexer<'a,Token>
+use super::tokens::Token;
+use logos::{Logos,Lexer};
+
+pub(in crate) struct TokenStream<'a>{
+    token_gen:Peekable<Lexer<'a,Token>>
 }
 
-impl TokenStream<'_>{
-    fn next(&self) -> Option<Token>{
+impl<'a> TokenStream<'a>{
+    pub(in crate::frontend) fn new(arg:Lexer<'a,Token>) -> Self{
+        Self{
+            token_gen:arg.into_iter().peekable()
+        }
+    }
+}
+
+impl<'a> Deref for TokenStream<'a>{
+    type Target = Peekable<Lexer<'a,Token>>;
+    fn deref(&self) -> &Self::Target {
+        &self.token_gen
+    }
+}
+
+enum AnalyzerStatuations{
+    ZeroSizeType,
+    Struct,
+    Enum,
+    Union
+}
+pub struct Analyzer<'a>{
+    toks:TokenStream<'a>,
+    construct:AnalyzerStatuations
+}
+
+impl Analyzer<'_>{
+    fn the_next(&mut self,expect:Token) -> bool{
+        self.toks.next().expect("unexpect EOF") == expect
+    }
+    fn try_construct_struct(&mut self) -> Option<crate::frontend::composite_type::StructDeclare>{
+        if !self.the_next(Token::Struct){
+            return None
+        }
         todo!()
-        // self.peek.or_else(||self.token_gen.next())
     }
 }
